5c6d0eb242c69220eff2302a85025fb191f3bb3c
Rogerio Angeliski <rogerio.angeliski@resultadosdigitais.com.br>
2021-03-03 18:53:34 -0300

    fix: add more strings

--- api.go
+++ api.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func Run() error{
 	fmt.Sprintf("Hello from go_auto_release!")
-	fmt.Sprintf("Patch added")
+	fmt.Sprintf("Patch added - Fix")
 	fmt.Sprintf("Minor added")
 	return nil
 }
