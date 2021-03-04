8889a84cd7a789f660bdefae5f4e7b1f6d0a988f
Rogerio Angeliski <rogerio.angeliski@resultadosdigitais.com.br>
2021-03-03 21:56:17 -0300

    fix: update major feature

--- api.go
+++ api.go
@@ -6,6 +6,6 @@ func Run() error{
 	fmt.Sprintf("Hello from go_auto_release!")
 	fmt.Sprintf("Patch added - Fix")
 	fmt.Sprintf("Minor added - Fix")
-	fmt.Sprintf("Major added :nervoso: -fix")
+	fmt.Sprintf("Major added :nervoso: -fix do fix")
 	return nil
 }
