669ef396c4a7931a6675328ab0f8de756cbda554
Rogerio Angeliski <rogerio.angeliski@resultadosdigitais.com.br>
2021-03-04 16:45:35 -0300

    feat: add a new feature

--- api.go
+++ api.go
@@ -2,10 +2,12 @@ package go_auto_release
 
 import "fmt"
 
-func Run() error{
+func Run() error {
 	fmt.Sprintf("Hello from go_auto_release!")
 	fmt.Sprintf("Patch added - Fix")
 	fmt.Sprintf("Minor added - Fix")
 	fmt.Sprintf("Major added :nervoso: -fix do fix")
+
+	fmt.Sprintf("New feature")
 	return nil
 }
